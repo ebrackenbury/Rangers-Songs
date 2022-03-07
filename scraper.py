3d8082991074d7dfea156b5e3fd9cbae91b9a71f
Evan Brackenbury <ebrack1@gmail.com>
2022-03-07 14:03:55 -0500

    defined games and links functions

--- scraper.py
+++ scraper.py
@@ -12,20 +12,53 @@ import re
 #     html_code = file.read()
 
 
-r = requests.get(url='https://www.nhl.com/rangers/info/sounds-of-the-game').text
+requested_html = requests.get(url='https://www.nhl.com/rangers/info/sounds-of-the-game')
 
-soup = BeautifulSoup(r, 'html.parser')
+# with open('rangers_games_html.html', 'w') as file:
+#     file.write(r.text)
 
-games = soup.find('table', class_='table-token__table stats_table')
+# with open('rangers_games_html.html', 'r') as file:
+#     html_code = file.read()
 
-links = soup.find_all('a', string='Click for Playlist')
 
-links_list = []
+def get_games(html):
+    soup = BeautifulSoup(html, 'html.parser')
 
-for link in links:
-    links_list.append(link.get("href"))
+    html_games = soup.find_all('td')
 
-print(links_list)
+    games_list = []
+
+    for game in html_games:
+        games_list.append(game.text)
+
+    games_list = [game for game in games_list if game not in "Click for Playlist"]
+
+    return games_list
+
+
+def get_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    html_links = soup.find_all('a', string='Click for Playlist')
+
+    links_list = []
+
+    for link in html_links:
+        links_list.append(link.get("href"))
+
+    return links_list
+
+
+games = get_games(requested_html)
+links = get_links(requested_html)
+
+games_dict = {"Game": games,
+              "Link": links}
+
+df = pd.DataFrame(data=games_dict)
+print(df)
+
+# df.to_csv("games.csv")
 
 # df = pd.read_html(str(games))[0]['DATE']
 
